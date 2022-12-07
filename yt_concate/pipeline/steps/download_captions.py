75b053aa398103fd8e728da997a2e381cf8722be
Johnson Hsu <specialline4107@gmail.com>
2022-12-07 20:48:58 +0800

    Finished multi-processing to download videos and download captions

--- yt_concate/pipeline/steps/download_captions.py
+++ yt_concate/pipeline/steps/download_captions.py
@@ -1,4 +1,5 @@
 import time
+from multiprocessing import Process
 
 from pytube import YouTube
 
@@ -10,12 +11,39 @@ class DownloadCaptions(Step):
     def process(self, data, inputs, utils):
         start = time.time()
 
+        # # use single thread to download captions
+        # for yt in data:
+        #     print('Downloading caption for', yt.id)
+        #     if utils.caption_file_exists(yt):
+        #         print(f'found existing file: {yt.url}')
+        #         continue
+        #
+        #     try:
+        #         source = YouTube(yt.url)
+        #         en_caption = source.captions.get_by_language_code('a.en')
+        #         en_caption_convert_to_srt = (en_caption.generate_srt_captions())
+        #     except (KeyError, AttributeError):
+        #         print('Error when downloading caption for', yt.url)
+        #         continue
+        #
+        #     text_file = open(yt.caption_filepath, "w", encoding='utf-8')
+        #     text_file.write(en_caption_convert_to_srt)
+        #     text_file.close()
+
+        # use multi-processing to download captions
+        self.activate_multiprocessing_captions(data, inputs, utils)
+
+        end = time.time()
+        print('Multi-processing downloading captions takes', end - start, 'seconds')
+
+        return data
+
+    def download_captions(self, data, inputs, utils):
         for yt in data:
             print('Downloading caption for', yt.id)
             if utils.caption_file_exists(yt):
                 print(f'found existing file: {yt.url}')
                 continue
-
             try:
                 source = YouTube(yt.url)
                 en_caption = source.captions.get_by_language_code('a.en')
@@ -28,7 +56,17 @@ class DownloadCaptions(Step):
             text_file.write(en_caption_convert_to_srt)
             text_file.close()
 
-        end = time.time()
-        print('took', end - start, 'seconds in downloading captions')
+    def activate_multiprocessing_captions(self, data, inputs, utils):
+        processes = []
+        section = int(len(data) / 4)
+        for i in range(4):
+            start = int(i * section)
+            end = int((i+1) * section)
+            processes.append(Process(target=self.download_captions, args=(data[start:end], inputs, utils)))
 
-        return data
+        for process in processes:
+            process.start()
+
+        for process in processes:
+            process.join()
+
--- yt_concate/pipeline/steps/download_videos.py
+++ yt_concate/pipeline/steps/download_videos.py
@@ -1,3 +1,7 @@
+import os
+from multiprocessing import Process
+import time
+
 from pytube import YouTube
 
 from .step import Step
@@ -6,8 +10,27 @@ from yt_concate.settings import VIDEOS_DIR
 
 class DownloadVideos(Step):
     def process(self, data, inputs, utils):
+        start = time.time()
         yt_set = set([found.yt for found in data])
         print('Videos to download:', len(yt_set))
+
+        # # use single thread to download videos
+        # for yt in yt_set:
+        #     url = yt.url
+        #     if utils.video_file_exists(yt):
+        #         print(f'Found existing video file for {url}, skipping')
+        #         continue
+        #     print('Downloading', url)
+        #     YouTube(url).streams.filter(res="720p").first().download(output_path=VIDEOS_DIR, filename=yt.id + '.mp4')
+
+        # use multiprocessing to download videos
+        self.activate_multiprocessing_videos(yt_set, inputs, utils)
+
+        end = time.time()
+        print('Multi-processing downloading videos takes', end - start, 'seconds')
+        return data
+
+    def download_videos(self, yt_set, inputs, utils):
         for yt in yt_set:
             url = yt.url
             if utils.video_file_exists(yt):
@@ -16,4 +39,16 @@ class DownloadVideos(Step):
             print('Downloading', url)
             YouTube(url).streams.filter(res="720p").first().download(output_path=VIDEOS_DIR, filename=yt.id + '.mp4')
 
-        return data
+    def activate_multiprocessing_videos(self, yt_set, inputs, utils):
+        processes = []
+        section = int(len(yt_set) / 4)
+        for i in range(4):
+            start = int(i * section)
+            end = int((i+1) * section)
+            processes.append(Process(target=self.download_videos, args=(list(yt_set)[start:end], inputs, utils)))
+
+        for process in processes:
+            process.start()
+
+        for process in processes:
+            process.join()
